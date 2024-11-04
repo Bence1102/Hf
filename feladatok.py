bbf2ad1052fd313ac73444199a967805691fde4e
Bence1102 <szente.bence04@gmail.com>
2024-11-04 18:46:56 +0100

    Hazi feladat

---
+++ feladatok.py
@@ -0,0 +1,70 @@
+import math
+import random
+
+def szamkiiras():
+    szam:int = int(input("Kérem adjon meg egy egész számot [200, 920] intervallumban: "))
+    if 200 <= szam <= 920:
+        elso_szamjegy = int(str(szam)[0])
+        print("A szám első számjegye:", elso_szamjegy)
+    else:
+        print("Hiba: A szám nincs a [200, 920] intervallumban!")
+    
+
+    
+
+def negyzetgyok():
+    szam:int = int(input("Adj meg egy valós számot: "))
+
+    if szam >= 0:
+        gyok_vonas = math.sqrt(szam)
+        print("A szám négyzetgyöke:", gyok_vonas)
+    else:
+        print("Hiba: Negatív számból nem lehet négyzetgyököt vonni!")
+
+
+def fel6():
+    szamok=[]
+    i=0
+    while i<13:
+        rszam:int=int(random.random()*17)-5 
+        szamok.append(int(rszam))
+        i+=1
+    return szamok
+def fel6_2(szamok):
+    negativ=len([rszam for rszam in szamok if rszam<0])
+    pozitiv=len([rszam for rszam in szamok if rszam>0])
+
+    return pozitiv, negativ
+def fel6_3(szamok):
+    ossz =0
+    for rszam in szamok:
+        if rszam %2==0:
+            ossz+=rszam
+    return ossz
+
+def fel6_4(szamok):
+    paratlan=0
+    paros=0
+    for rszam in szamok:
+        if rszam %2==0:
+            paros+=rszam
+        else:
+            paratlan+=rszam
+    return paratlan, paros
+
+def fel6_5(szamok):
+    atlag=0
+    for rszam in szamok:
+        atlag+=rszam
+    if len(szamok)==0:
+        return 0
+    return atlag/len(szamok)
+
+def fel6_6(szamok):
+    if len(szamok)==0:
+        return
+    max=szamok[0]
+    for rszam in szamok:
+        if rszam>max:
+            max=rszam
+    return max
---
+++ main.py
@@ -0,0 +1,30 @@
+import feladatok
+import random
+import math
+
+
+szamkiiras=feladatok.szamkiiras()
+print(szamkiiras)
+
+gyok=feladatok.negyzetgyok()
+print(gyok)
+
+randomsz=feladatok.fel6()
+print(randomsz)
+
+pozitiv, negativ=feladatok.fel6_2(randomsz)
+print("Pozitív számok:", pozitiv)
+print("Negatív számok:",negativ)
+
+ossz=feladatok.fel6_3(randomsz)
+print("A páros számok összege:", ossz)
+
+paratlan, paros=feladatok.fel6_4(randomsz)
+print("Páros számok összege:",paros)
+print("Páratlan számok összege:", paratlan)
+
+atlag=feladatok.fel6_5(randomsz)
+print("Átlag:", atlag)
+
+max=feladatok.fel6_6(randomsz)
+print("A legnagyobb szám: ", max)
